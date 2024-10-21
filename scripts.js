4a1d43d65ea8f8575a57f46389bce292e16fa3a9
KamilMiarecki <92676433+KamilMiarecki@users.noreply.github.com>
2024-10-21 21:12:28 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-
 // Get the button
 let mybutton = document.getElementById("myBtn");
 
@@ -21,8 +20,8 @@ function topFunction() {
 }
 
 
-// Tablica obrazów
-var images = [
+// Image array
+const images = [
   "images_for_inspiration/1.jpg",
   "images_for_inspiration/2.jpg",
   "images_for_inspiration/3.jpg",
@@ -33,89 +32,67 @@ var images = [
   "images_for_inspiration/8.jpg",
 ];
 
-var currentIndex = 0; // Bieżący indeks obrazu
+let currentIndex = 0; // Current image index
 
-// Funkcja otwierająca obraz i ustawiająca indeks
-function openImage(index) {
+// Open image in modal
+const openImage = index => {
   currentIndex = index;
-  var modal = document.getElementById("imageModal");
-  var modalImg = document.getElementById("modal-img");
-  
+  const modal = document.getElementById("imageModal");
+  const modalImg = document.getElementById("modal-img");
+
   modal.style.display = "block";
   modalImg.src = images[currentIndex];
 }
 
-// Funkcja zamykająca modal
-function closeImage() {
-  var modal = document.getElementById("imageModal");
+// Close modal
+const closeImage = () => {
+  const modal = document.getElementById("imageModal");
   modal.style.display = "none";
 }
 
-// Funkcja zmieniająca obraz na kolejny lub poprzedni
-function changeImage(direction) {
-  currentIndex += direction;
-
-  // Sprawdzenie, czy indeks nie wyszedł poza zakres
-  if (currentIndex >= images.length) {
-      currentIndex = 0; // Jeśli ostatni obraz, wróć na pierwszy
-  } else if (currentIndex < 0) {
-      currentIndex = images.length - 1; // Jeśli pierwszy obraz, wróć na ostatni
-  }
-
-  var modalImg = document.getElementById("modal-img");
-  modalImg.src = images[currentIndex];
+// Change image
+const changeImage = direction => {
+  currentIndex = (currentIndex + direction + images.length) % images.length; // Looping index
+  document.getElementById("modal-img").src = images[currentIndex];
 }
 
-
-
-function autoResize(textarea) {
-    textarea.style.height = 'auto'; // Resetuje wysokość, aby zmierzyć nową wysokość
-    textarea.style.height = textarea.scrollHeight + 'px'; // Ustawia wysokość na scrollHeight
+// Auto resize textarea
+const autoResize = textarea => {
+  textarea.style.height = 'auto'; // Reset height
+  textarea.style.height = `${textarea.scrollHeight}px`; // Set height to scrollHeight
 }
-// Inicjalizacja funkcji, aby dostosować wysokość na załadowanie strony
-document.addEventListener('DOMContentLoaded', function() {
-  const textareas = document.querySelectorAll('textarea');
-  textareas.forEach(textarea => {
-      autoResize(textarea);
-  });
+
+// Initialize auto resize for textareas
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('textarea').forEach(autoResize);
 });
 
-
-function scrollToContact() {
-  const contactSection = document.getElementById('kontakt');
-  contactSection.scrollIntoView({ behavior: 'smooth' });
-}
-function toggleMenu() {
-  const navLinks = document.querySelector('.nav__links');
-  navLinks.classList.toggle('active'); // Dodaj lub usuń klasę 'active'
+// Scroll to contact section smoothly
+const scrollToContact = () => {
+  document.getElementById('kontakt').scrollIntoView({ behavior: 'smooth' });
 }
 
+// Toggle navigation menu
+const toggleMenu = () => {
+  document.querySelector('.nav__links').classList.toggle('active');
+}
+
+// Close menu on link click
 document.querySelectorAll('.nav__links a').forEach(link => {
   link.addEventListener('click', () => {
-      const navLinks = document.querySelector('.nav__links');
-      navLinks.classList.remove('active'); // Zamknij menu po kliknięciu
+    document.querySelector('.nav__links').classList.remove('active');
   });
 });
 
-
-
-
-// Themes begin
+// Initialize map chart with amCharts
 am4core.useTheme(am4themes_animated);
-// Themes end
-
-// Tworzymy instancję mapy
-var chart = am4core.create("chartdiv", am4maps.MapChart);
-
-// Ustawiamy projekcję mapy
+const chart = am4core.create("chartdiv", am4maps.MapChart);
 chart.projection = new am4maps.projections.Miller();
-
-// Włączamy przesuwanie i zoomowanie
 chart.panBehavior = "move"; 
 chart.zoomControl = new am4maps.ZoomControl();
 
-// Definiujemy listę odwiedzonych krajów i przypisane im kolory
-var visitedCountries = [{ "id": "FR", "name": "France", "color": am4core.color("#ff0000") },  // Czerwony
+const visitedCountries = [
+  { "id": "FR", "name": "France", "color": am4core.color("#ff0000") },  // Czerwony
   { "id": "IT", "name": "Italy", "color": am4core.color("#0000ff") },   // Niebieski
   { "id": "ES", "name": "Spain", "color": am4core.color("#ffff00") },   // Żółty
   { "id": "PL", "name": "Poland", "color": am4core.color("#ffa500") },  // Pomarańczowy
@@ -143,70 +120,40 @@ var visitedCountries = [{ "id": "FR", "name": "France", "color": am4core.color("
   { "id": "CH", "name": "Switzerland", "color": am4core.color("#ff1493") }, // Jasnoróżowy
   { "id": "UA", "name": "Ukraine", "color": am4core.color("#adff2f") }, // Zielonożółty
   { "id": "GB", "name": "United Kingdom", "color": am4core.color("#191970") } // Midnight Blue
-  
 ];
 
-// Tworzymy warstwę dla mapy świata
-var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+// Create map layer
+const polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
 polygonSeries.useGeodata = true;
 polygonSeries.geodata = am4geodata_worldLow;
 
-// Ustawiamy domyślny kolor krajów na szary
-var polygonTemplate = polygonSeries.mapPolygons.template;
-polygonTemplate.fill = am4core.color("#aaaaaa"); // Szary kolor
-
-// Dodajemy tooltip z nazwą kraju
+const polygonTemplate = polygonSeries.mapPolygons.template;
+polygonTemplate.fill = am4core.color("#aaaaaa");
 polygonTemplate.tooltipText = "{name}";
 polygonTemplate.nonScalingStroke = true;
 polygonTemplate.strokeWidth = 0.5;
 
-// Stany hover i active dla podświetlania krajów
-var hs = polygonTemplate.states.create("hover");
-hs.properties.fill = am4core.color("#aaaaaa"); // Kolor podświetlenia na hover
+// Hover state
+const hs = polygonTemplate.states.create("hover");
+hs.properties.fill = am4core.color("#aaaaaa");
 
-var activeState = polygonTemplate.states.create("active");
-activeState.properties.fill = am4core.color("#aaaaaa"); // Kolor po kliknięciu
-
-// Tablica, aby przechować odwiedzone kraje dla późniejszego zoomu
-var visitedPolygons = [];
-
-// Tworzymy serię dla każdego odwiedzonego kraju z przypisanym kolorem
-visitedCountries.forEach(function(country) {
-  var countrySeries = chart.series.push(new am4maps.MapPolygonSeries());
-
-  // Ustawiamy mapę dla każdego kraju
+visitedCountries.forEach(country => {
+  const countrySeries = chart.series.push(new am4maps.MapPolygonSeries());
   countrySeries.geodata = am4geodata_worldLow;
-  countrySeries.include = [country.id]; // Kod kraju
-  
-  // Ustawiamy tooltip, kolor i efekt hover
-  var countryPolygon = countrySeries.mapPolygons.template;
+  countrySeries.include = [country.id];
+
+  const countryPolygon = countrySeries.mapPolygons.template;
   countryPolygon.fill = country.color;
-  countryPolygon.tooltipText = "[bold]{name}[/]"; // Nazwa kraju w tooltip
+  countryPolygon.tooltipText = `[bold]{name}[/]`;
   countryPolygon.strokeWidth = 2;
-  
-  // Stan hover dla podświetlenia odwiedzonych krajów na ciemniejszy kolor
-  var hoverState = countryPolygon.states.create("hover");
-  hoverState.properties.fill = am4core.color(am4core.colors.brighten(country.color.rgb, -0.3)); // Ciemniejszy kolor na hover
 
-  // Animacja dla odwiedzonego kraju
-  countryPolygon.events.on("inited", function(event) {
+  const hoverState = countryPolygon.states.create("hover");
+  hoverState.properties.fill = am4core.color(am4core.colors.brighten(country.color.rgb, -0.3));
+
+  countryPolygon.events.on("inited", event => {
     event.target.animate({ property: "fill", to: country.color, duration: 1000, easing: am4core.ease.linear });
-  });
-
-  // Zapisujemy odwiedzone kraje do listy visitedPolygons, aby móc na nie później zoomować
-  countrySeries.events.on("inited", function() {
-    visitedPolygons.push(countrySeries);
-    if (visitedPolygons.length === visitedCountries.length) {
-      zoomToVisitedCountries();
-    }
   });
 });
 
-// Funkcja do zoomowania na odwiedzone kraje
-function zoomToVisitedCountries() {
-  var bounds = chart.getGeodataBounds(visitedPolygons.map(series => series.mapPolygons.getIndex(0)));
 
-  if (bounds) {
-    chart.zoomToRectangle(bounds.north, bounds.east, bounds.south, bounds.west, 1, true);
-  }
-}
+
